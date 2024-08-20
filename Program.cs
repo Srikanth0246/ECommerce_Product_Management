6a39f0839623bf8f14c19f7010d061f4277bd23e
Prashanth6340 <ganegoniprashanth@gmail.com>
2024-08-20 16:43:47 +0530

    saved

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿internal class Program
 {
+    public static void TestProductManagement()
+    {
+        var productManager = new ProductManager();
+        var category = new Category { Id = 1, Name = "Electronics" };
+        productManager.AddCategory(category);
+    
+        var product = new Electronics { Id = 1, Name = "Laptop", Price = 1000, CategoryId = 1, Brand = "BrandA", Model = "ModelX" };
+        productManager.AddProduct(product);
+    
+        var retrievedProduct = productManager.SearchProduct(1);
+        Console.WriteLine($"Product: {retrievedProduct.Name}, Price: {retrievedProduct.Price}");
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        TestProductManagement();
     }
 }
--- obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64009ab364a41e85778a3bee833af1e9e3395ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91aec02a4fd5b81cbe302b3bb813d9e538858c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
