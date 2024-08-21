4b42f1d6a146d7f6507e04c44c9d79b13848bc2a
Srikanth0246 <tippasanisrikanth9966@gmail.com>
2024-08-21 18:11:05 +0530

    saved

--- Exception.cs
+++ Exception.cs
@@ -8,10 +8,7 @@ public class CategoryNotFoundException : Exception
     public CategoryNotFoundException(string message) : base(message) { }
 }
 
-public class DuplicateProductException : Exception
-{
-    public DuplicateProductException(string message) : base(message) { }
-}
+
 public class ProductNotFoundException : Exception
 {
     public ProductNotFoundException(string message) : base(message) { }
--- obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46339226b0bdf580bde070892d06e4317e4df94d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f680d3144decdc7f351eabd84592d327c3dd3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
